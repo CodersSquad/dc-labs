674e14290415df8119e82f071d8407738f383b6a
Fabian Adolfo Cervantes Romero <0209134@up.edu.mx>
2020-01-29 09:18:59 -0600

    Tarea 2
    Signed-off-by: Fabian Adolfo Cervantes Romero <0209134@up.edu.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	t := map[string]int{}
+	p := strings.Split(s, " ")
+	for _,word := range p{
+		t[word]++
+	}
+	
+	
+	return t
+	
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,21 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	im :=make([][]uint8,dy)
+	for i:=0;i<dy; i++{
+		im[i]=make([]uint8,dx)
+	}
+	
+	for y,_ := range im{
+	for x,_:= range im[y]{
+		im[y][x]=uint8(x+y)
+		}
+		}
+	return im
+}
+
 func main() {
+	pic.Show(Pic)
 }
