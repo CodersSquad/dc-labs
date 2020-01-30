5dfe55cb3906919c4dbc51934efc9bc84ff91831
cubomx <cheservelram657@gmail.com>
2020-01-29 18:41:03 -0600

    Already solve my lab
    Signed-off-by: cubomx <cheservelram657@gmail.com>

---
+++ labs/first-challenge/main.py
@@ -0,0 +1,45 @@
+word = input()
+
+substrings = set()
+
+new_word = ""
+
+longest = ""
+
+start = 0
+
+position = 0
+
+while position < len(word):
+
+  old_length = len(substrings)
+
+  substrings.add(word[position])
+
+  new_length = len(substrings)
+
+  if old_length == new_length or position + 1 == len(word):
+
+    print(new_word)
+
+    if len(new_word) > len(longest):
+
+      longest = new_word
+
+    start = start + 1
+
+    position = start
+
+    new_word = ""
+
+    substrings = set()
+
+  else:
+
+    new_word += word[position]
+
+    position += 1
+
+
+
+print (len(longest))
